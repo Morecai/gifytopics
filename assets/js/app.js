b93d4fa06b63e8f30604db8b876c548b1b5ae9ca
Frank Mcdonald <frankmcd@cox.net>
2017-05-22 16:19:12 -0700

    rearranged js file layout to fix click function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,75 @@
-        var topics = ["BBQ", "scuba", "cats", "ferrets", "rpg", "guns", "star wars", "star trek"];
+      $(document).ready(function() {
+          var topics = ["BBQ", "scuba", "cats", "ferrets", "rpg", "guns", "star wars", "star trek", "sharks", "ufo", "stunts", "bacon"];
 
 
-        function displayTopicInfo() {
-            var topic = $(this).attr("data-name");
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-                topic + "&api_key=dc6zaTOxFJmzC&limit=10";
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).done(function(response) {
-                $("#topicInfo").text(JSON.stringify(response));;
-                var results = response.data;
-                for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div class='item'>");
-                    var topic = $(".gif").addClass("data-state");
-                    var rating = results[i].rating;
+          function displayTopicInfo() {
+              var topic = $(this).attr("data-name");
+              var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+                  topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+              $.ajax({
+                  url: queryURL,
+                  method: "GET"
+              }).done(function(response) {
+                  $("#topicInfo").text(JSON.stringify(response));
+                  var results = response.data;
+                  console.log(results);
+                  for (var i = 0; i < results.length; i++) {
+                      var gifDiv = $("<div class='item'>");
+                      var rating = results[i].rating;
 
-                    var p = $("<p>").text("Rating: " + rating);
+                      var p = $("<p>").text("Rating: " + rating);
 
-                    var topicImage = $("<img>");
-                    topicImage.attr("src", results[i].images.fixed_height_still.url);
+                      var topicImage = $("<img class='gif'>");
+                      topicImage.attr("src", results[i].images.fixed_height.url);
+                      topicImage.attr("data-animate", results[i].images.fixed_height.url);
+                      topicImage.attr("data-still", results[i].images.fixed_height_still.url);
 
-                    gifDiv.prepend(p);
-                    gifDiv.prepend(topicImage);
+                      gifDiv.prepend(p);
+                      gifDiv.prepend(topicImage);
 
-                    $("#gifs-appear-here").prepend(gifDiv);
-                }
-            });
-        }
+                      $("#gifs-appear-here").prepend(gifDiv);
+                  }
 
-        function renderButtons() {
+                  $(".gif").on("click", function() {
 
-            $("#topics-view").empty();
+                      var state = $(this).attr("data-state");
+                      if (state === "still") {
+                          $(this).attr("src", $(this).attr("data-animate"));
+                          $(this).attr("data-state", "animate");
+                      } else {
+                          $(this).attr("src", $(this).attr("data-still"));
+                          $(this).attr("data-state", "still");
 
-            for (var i = 0; i < topics.length; i++) {
+                      }
+                  });
+              });
+          }
 
-                var a = $("<button>");
-                a.addClass("topic");
-                a.attr("data-name", topics[i]);
-                a.text(topics[i]);
-                $("#topics-view").append(a);
-            }
-        }
+          function renderButtons() {
 
-        $("#add-topic").on("click", function(e) {
+              $("#topics-view").empty();
 
-            e.preventDefault();
-            console.log(e);
+              for (var i = 0; i < topics.length; i++) {
 
-            var topic = $("#topic-input").val().trim();
-            topics.push(topic);
+                  var a = $("<button>");
+                  a.addClass("topic");
+                  a.attr("data-name", topics[i]);
+                  a.text(topics[i]);
+                  $("#topics-view").append(a);
+              }
+          }
 
-            renderButtons();
-        });
-        $(document).on("click", ".topic", displayTopicInfo);
-        renderButtons();
+          $("#add-topic").on("click", function(e) {
 
-        $(".gif").on("click", function() {
+              e.preventDefault();
+              console.log(e);
 
-            var state = $(this).attr("data-state");
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("animate"));
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", $(this).attr("still"));
-                $(this).attr("data-state", "still");
+              var topic = $("#topic-input").val().trim();
+              topics.push(topic);
 
-            }
-        });
+              renderButtons();
+          });
+          $(document).on("click", ".topic", displayTopicInfo);
+          renderButtons();
+
+      });
